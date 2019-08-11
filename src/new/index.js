e38c1ef4301bedfb24f17fb2aabadf9a6aa17096
littleLane <littleLaneBye@gmail.com>
2019-08-11 22:51:32 +0800

    feat: new

--- src/new/index.js
+++ src/new/index.js
@@ -1,17 +1,20 @@
-function new1(func) {
-  // 模拟 this 对象
-  const obj = {}
-
-  // 如果 func 有原型对象，就将原型对象设置给 obj 的原型
-  if (func.prototype !== null) {
-  	Object.setPrototypeOf(obj, func.prototype)
+function myNew(func) {
+  // 判断第一个参数是否为构造函数，不是就抛出一个类型错误
+  if (typeof func !== 'function') {
+    throw TypeError(`${func} is not a constructor`)
   }
 
-  const result = func.apply(obj, Array.prototype.slice.call(arguments, 1))
+  // 新建一个空对象
+  var ctx = {}
 
-  // 如果函数逻辑有显式的 return ，那就直接返回 return 的值
-  // 如果没有显式的 return，就返回 obj
-  return result instanceof Object ? result : obj
+  // 将构造函数的 prototype 原型对象赋值给新对象的 __proto__ 原型
+  if (func.prototype) {
+    Object.setPrototypeOf(ctx, func.prototype)
+  }
+
+  // 以 ctx 为上下文并传入额外的参数执行函数
+  var result = func.apply(ctx, Array.prototype.slice.call(arguments, 1))
+  
+  // 执行函数的结果为对象就返回结果，否则返回新创建的对象
+  return result instanceof Object ? result : ctx;
 }
-
-module.exports = new1
