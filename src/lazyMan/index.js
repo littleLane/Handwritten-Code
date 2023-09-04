1d935e4a8e9700f66deb6a7277dfe1dd708248d0
littleLane <lzz857183384@163.com>
2023-09-04 14:50:51 +0800

    lazyMan

--- src/lazyMan/index.js
+++ src/lazyMan/index.js
@@ -22,60 +22,61 @@
  * @param {*} name 
  */
 function LazyMan(name) {
-  return new LazyClass(name)
+  return new LazyClass(name);
 }
 
 class LazyClass {
   constructor(name) {
-    this.tasks = []
+    this.tasks = [];
 
     this.tasks.push(() => {
-      console.log(`Hello I'm ${name}`)
-      this.next()
-    })
+      console.log(`Hello I'm ${name}`);
+      this.next();
+    });
 
     // 这里 this.tasks 的第一个任务是上面的任务，如果同步执行就只会执行上面的任务，后面的所有任务都不会执行到
     // 由于 setTimeout 的特性，会在下一轮 event loop 时执行，所以这里的 this.next 是在所有任务排入 this.task 后执行的
     // 此时任务队列是业务逻辑的任务队列
+    // 等待所有任务入队后执行任务
     setTimeout(() => {
-      this.next()
-    }, 0)
+      this.next();
+    }, 0);
   }
 
   sleep(time) {
     this.tasks.push(() => {
-      console.log(`Sleeping ${time}`)
+      console.log(`Sleeping ${time}`);
       setTimeout(() => {
-        console.log(`Wake up after ${time}`)
-        this.next()
-      }, time)
-    })
-    return this
+        console.log(`Wake up after ${time}`);
+        this.next();
+      }, time);
+    });
+    return this;
   }
 
   sleepFirst(time) {
     this.tasks.unshift(() => {
-      console.log(`Sleeping ${time}`)
+      console.log(`Sleeping ${time}`);
       setTimeout(() => {
-        console.log(`Wake up after ${time}`)
-        this.next()
-      }, time)
-    })
-    return this
+        console.log(`Wake up after ${time}`);
+        this.next();
+      }, time);
+    });
+    return this;
   }
 
   eat(something) {
     this.tasks.push(() => {
-      console.log("Eat "+something);
+      console.log("Eat " + something);
       this.next();
-    })
-    return this
+    });
+    return this;
   }
 
   next() {
-    const task = this.tasks.shift()
-    task && task()
+    const task = this.tasks.shift();
+    task && task();
   }
 }
 
-LazyMan("Joe").sleepFirst(3000).eat("breakfast").sleep(1000).eat("dinner")
+LazyMan("Joe").sleepFirst(3000).eat("breakfast").sleep(1000).eat("dinner");
