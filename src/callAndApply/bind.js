597a4558cbb5adc90ed7efa0d3c5b3fd8efc3ce3
littleLane <lzz857183384@163.com>
2023-09-11 13:37:15 +0800

    bind

--- src/callAndApply/bind.js
+++ src/callAndApply/bind.js
@@ -17,7 +17,7 @@ Function.prototype.bind2 = function (ctx) {
     // 1、如果是通过 new 进行调用，this 就是指向实例，this 的原型链上就会包含 fBound 的 prototype 原型对象，
     // 此时最终的函数绑定的上下文就不是原来传递的上下文了，而是新创建的实例
     // 2、如果是通过普通函数进行调用，就会按原先的绑定方式绑定上下文
-    self.apply(this instanceof fNOP ? this : ctx, args);
+    return self.apply(this instanceof fNOP ? this : ctx, args);
   };
 
   if (self.prototype) {
